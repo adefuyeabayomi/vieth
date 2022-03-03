13d55ac7b15303d257b7f0b6c674083f9ea56bf9
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2022-03-03 15:24:37 +0100

    Add files via upload

--- front-end/pages-components/transactions/buy-smile-data.js
+++ front-end/pages-components/transactions/buy-smile-data.js
@@ -4,16 +4,16 @@ let obj = {
         let priceObj = price;
         return {
             priceObj : priceObj,
-            dataVolume : "",
+            dataVolume : "igig",
             smileID : "",
             paymentChannel : "",
         }
     },
     computed : {
         price : function(){
-            return this.dataVolume == "" ? "" :  this.priceObj[this.dataVolume];
+            return this.priceObj[this.dataVolume];
         }
     },
-    template : "         <div id=\"buy_smile_data_section\">            <div class=\"std_header_1\">                <p>Buy Smile Data Subscription</p>            </div>               <div class=\"left-pad-std\">                <p  class=\"p_header_std\">Service Provider Smile NG</p>                <p  class=\"p_header_std\">Amount :</p>                <div id=\"smile-amount\">                    <select  class=\"input_std\" v-model=\"dataVolume\" name=\"smile-amount\">                        <option disabled value=\"\">--select--</option>                        <option value=\"1gig\">1GB</option>                    </select>                </div>            </div>            <div class=\"left-pad-std\">                <p>Price : NGN {{price}}</p>            </div>            <div class=\"left-pad-std\">                <p  class=\"p_header_std\">Please input your smile id : </p>                <input class=\"input_std\" type=\"text\" v-model=\"smileID\" placeholder=\"...\">            </div>            <div class=\"left-pad-std\" id=\"pay\">                <p  class=\"p_header_std\">How are you paying : </p>                <form>                    <input v-model=\"paymentChannel\" type=\"radio\" id=\"directly\" name=\"payment_channel\" checked> Directly                     <input v-model=\"paymentChannel\" type=\"radio\" id=\"wallet\" name=\"payment_channel\"> From Wallet                 </form>            </div>            <div class=\"button_container_std\">                <button>Proceed <span class=\"fas fa-arrow-right\"></span></button>            </div>            <div id=\"modal_bg\">                <div class=\"loading modal\">                    <p id=\"rolling_anim\"><span class=\"fas fa-half-round-arrow\"></span></p>                </div>            </div>        </div>         "
+    template : "         <div id='buy_smile_data_section'>            <div>                <p class='std_header_1'>Buy Smile Data Subscription</p>            </div>               <div>                <p>Service Provider Smile NG</p>                <p>Amount :</p>                <div id='smile-amount'>                    <select v-model='dataVolume' name='smile-amount'>                        <option value='1gig'>1GB</option>                    </select>                </div>            </div>            <div class='price_container'>                <p>Price : NGN {{price}}</p>            </div>            <div id='smileIDContainer'>                <p class='sidheader'>Please input your smile id : </p>                <input type='text' v-model='smileID' placeholder='...' class='sidinput'>            </div>            <div id='pay'>                <p>How are you paying : </p>                <form>                    <input v-model='paymentChannel' type='radio' id='directly' name='payment_channel' checked> Directly                     <input v-model='paymentChannel' type='radio' id='wallet' name='payment_channel'> From Wallet                 </form>            </div>            <div class='proceed_button_container'>                <button>Proceed</button>            </div>            <div id='modal_bg'>                <div class='loading modal'>                    <p id='rolling_anim'><span class='fas fa-half-round-arrow'></span></p>                </div>            </div>        </div>         "
 }
 export default obj;
