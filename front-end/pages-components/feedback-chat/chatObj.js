293b17de17bdb980b775f65d0a7fe8436adf4935
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2022-03-03 15:05:19 +0100

    Add files via upload

---
+++ front-end/pages-components/feedback-chat/chatObj.js
@@ -0,0 +1,9 @@
+let obj = [
+    {client : true,admin : false,message : "good Evening, i want to make an enquiry"},
+    {client : false,admin : true,message : "Ok Ma'm"},
+    {client : false,admin : true,message : "What can i help you with"},
+    {client : true,admin : false,message : "good Evening, i want to make an enquiry"}
+];
+export default obj;
+
+
