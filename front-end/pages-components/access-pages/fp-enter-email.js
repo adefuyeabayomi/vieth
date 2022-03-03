a590b14a369f0fe6a71548974f9b1ba8a764f534
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2022-03-03 15:23:02 +0100

    Add files via upload

--- front-end/pages-components/access-pages/fp-enter-email.js
+++ front-end/pages-components/access-pages/fp-enter-email.js
@@ -1,25 +1,10 @@
 let obj = {
-    data : function (){
+    data : function(){
         return {
             email : "",
-            loading : true,
-            requestSent:  false,
-            sent : false,
+            notDone : false,
         }
     },
-    methods  : {
-        closeModal : function(){
-            this.requestSent = false;
-        },
-        sendRequest : function(){
-            this.requestSent=true;
-            this.loading = true;
-            setTimeout(()=>{
-                this.loading = false;
-                this.sent = true;
-            }, 2000)
-        }
-    },
-    template : "         <div class=\"password_recovery_page\">            <div class=\"std_header_1\">                <p >Recover Your Password</p>            </div>            <div>                <form>                    <div>                        <p>Please input the email you used to open the account below</p>                        <input v-model=\"email\" type=\"email\" placeholder=\" Email :\">                    </div>                    <div class=\"submit_container\">                        <button v-on:click=\"sendRequest()\">Reset Password</button>                    </div>                </form>            </div>            <div v-if=\"requestSent\" class=\"success-modal modal_bg\">                <div v-if=\"loading\" class=\"modal_container_loading\">                    <div id=\"loading-box\">                        <div class=\"ui active loader\"></div>                    </div>                </div>                <div v-if=\"sent\" class=\"modal_container_info\">                    <div>We have Emailed you a new password, please check your email and login using that password. We advice you to change it using the user setting provided on this site. Thank you</div>                    <div class=\"cta_modal_cont\">                        <button v-on:click=\"closeModal()\">OK, Got that! <span class=\"fas fa-thumbs-up\"></span></button>                    </div>                </div>            </div>        </div>         "
+    template : "         <div class='password_recovery_page'>            <div>                <p class='std_header_1'>Recover Your Password</p>            </div>            <div>                <form>                    <div>                        <p>Please input the email you used to open the account below</p>                        <input v-model='email' type='email' placeholder=' Email :'>                    </div>                    <div class='submit_container'>                        <button>Reset Password</button>                    </div>                </form>            </div>            <div v-bind:class='{hidden: notDone}' class='success-modal modal_bg'>                <div class='modal_container'>                    <div>We have Emailed you a new password, please check your email and login using that password. We advice you to change it using the user setting provided on this site. Thank you</div>                    <div class='cta_modal_cont'>                        <button>OK, Got that! <span class='fas fa-thumbs-up'></span></button>                    </div>                </div>            </div>        </div>         "
 }
 export default obj;
--- front-end/pages-components/admin-front-end/access-adm.js
+++ front-end/pages-components/admin-front-end/access-adm.js
@@ -1,18 +1,9 @@
 let obj = {
     data : function (){
         return {
-            emailVerified : false,
-            email : "",
-            otp  :"",
+            admPassword : ""
         }
     },
-    methods: {
-        authenticate(){
-            if(this.emailVerified){
-                //send otp,
-            }
-        }
-    },
-    template : "         <div class=\"std_header_1\">             <p>Admin Login</p>            <div v-if=\"!emailVerified\" class=\"left-pad-std\">                <p>Email</p>                <input class=\"input_std\" v-model=\"email\" type=\"email\">                <div class=\"button_container_std\">                    <button v-on:click=\"authenticate()\" class=\"adm-login\">Proceed <span class=\"fas fa-arrow-right\"></span></button>                </div>            </div>            <div v-if=\"emailVerified\" class=\"left-pad-std\">                <p class=\"p_header_std\">Enter OTP</p>                <input class=\"input_std\" v-model=\"otp\" type=\"number\">                <div class=\"button_container_std\">                    <button v-on:click=\"authenticate()\" class=\"adm-login\">Proceed <span class=\"fas fa-arrow-right\"></span></button>                </div>            </div>                               </div>         "
+    template : "         <div>            <p>Admin Login</p>            <input v-model='admPassword'  type='password'>           <button id='admLoginButton'>Login</button>      </div>         "
 }
 export default obj;
